1f8fa6e4a7781a484b2e46b3062ed1217a711878
Edwin Huang <edwinwhuang@gmail.com>
2017-08-17 21:57:02 -0700

    begin unequal-time measurements

--- src/meas.c
+++ src/meas.c
@@ -24,7 +24,7 @@ void measure_eqlt(const struct params *const restrict p, const int sign,
 	for (int i = 0; i < N; i++) {
 		const int delta = (i == j);
 		const int r = p->map_ij[i + j*N];
-		const int pre = (double)sign / p->degen_ij[r];
+		const double pre = (double)sign / p->degen_ij[r];
 		const double guii = gu[i + i*N], gdii = gd[i + i*N];
 		const double guij = gu[i + j*N], gdij = gd[i + j*N];
 		const double guji = gu[j + i*N], gdji = gd[j + i*N];
@@ -47,4 +47,20 @@ void measure_uneqlt(const struct params *const restrict p, const int sign,
 {
 	m->n_sample++;
 	m->sign += sign;
+	const int N = p->N, L = p->L, num_i = p->num_i, num_ij = p->num_ij;
+	const int NL = N*L;
+
+	// 2-site measurements
+	for (int l = 0; l < L; l++)
+	for (int j = 0; j < N; j++)
+	for (int k = 0; k < L; k++)
+	for (int i = 0; i < N; i++) {
+		const int t = (L + k - l) % L;
+		const int r = p->map_ij[i + j*N];
+		const int t_sign = ((k >= l) ? 1.0 : -1.0);  // for fermionic
+		const double pre = (double)sign / p->degen_ij[r] / L;
+		const double Guij = Gu[(i + N*k) + NL*(j + N*l)];
+		const double Gdij = Gd[(i + N*k) + NL*(j + N*l)];
+		m->gt0[r + N*t] += 0.5*t_sign*pre*(Guij + Gdij);
+	}
 }
