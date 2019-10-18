d9a10fadc4e60773ad4fa1bec18ab83667a76257
Edwin Huang <edwinwhuang@gmail.com>
2019-10-17 19:23:56 -0700

    support jackknife resampling of non-i.i.d. data

--- util/util.py
+++ util/util.py
@@ -29,6 +29,12 @@ def load(path, *args):
 
 
 def jackknife(*args, f=lambda s, sx: (sx.T/s.T).T.real):
+    '''
+    jackknife resampling assuming i.i.d. bins.
+    positional arguments should be arrays, with first index for bin index.
+    the keyword argument f is a function that defines the desired statistic in
+        terms of the arguments, summed over the bin index.
+    '''
     n = args[0].shape[0]
     sums = tuple(a.sum(0) for a in args)
     res_all = f(*sums)
@@ -38,3 +44,24 @@ def jackknife(*args, f=lambda s, sx: (sx.T/s.T).T.real):
     res_jk_mean = res_jk.mean(0)
     res_jk_var = ((res_jk - res_jk_mean)**2).mean(0)
     return np.stack(((n*res_all - (n-1)*res_jk_mean), ((n-1)*res_jk_var)**0.5))
+
+
+def jackknife_noniid(*args, f=lambda n, ns, nsx: (nsx.T/ns.T).T.real):
+    '''
+    jackknife resampling assuming independent and non-identical bins.
+    positional arguments should be arrays, with first index for bin index.
+    the first argument must be equal (or proportional) to the number of samples
+    in each bin.
+    the keyword argument f is a function that defines the desired statistic in
+    terms of the arguments, summed over the bin index.
+    '''
+    nbin, = args[0].shape
+    sums = tuple(a.sum(0) for a in args)
+    res_all = f(*sums)
+    if nbin == 1:
+        return np.stack((res_all, np.zeros_like(res_all)))
+    res_jk = f(*(s - a for s, a in zip(sums, args)))
+    m = sums[0] - args[0]
+    res_jk_mean = (m * res_jk.T).T.sum(0)/m.sum()
+    res_jk_var = (m*m/args[0] * ((res_jk - res_jk_mean)**2).T).T.sum(0)/m.sum()
+    return np.stack((res_all, res_jk_var**0.5))
