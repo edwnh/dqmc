45cdf436e3a0f3462a2217bb8c0fc3379c7f7556
Edwin Huang <edwinwhuang@gmail.com>
2024-05-23 04:09:24 -0400

    add -c option to print cpu model

--- src/main_1.c
+++ src/main_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #ifdef _OPENMP
 	#include <omp.h>
@@ -14,7 +15,33 @@
 
 static void usage(const char *name)
 {
-	printf("usage: %s [-b] [-l log_file.log] [-s save_interval] [-t max_time] sim_file.h5\n", name);
+	printf("usage: %s [-b] [-c] [-l file.log] [-s interval] [-t max_time] sim_file.h5\n"
+	       "options:\n"
+	       "  -b           set benchmarking mode. no data is saved.\n"
+	       "  -c           print cpu model from /proc/cpuinfo.\n"
+	       "  -l file.log  output to log file instead of stdout.\n"
+	       "  -s interval  time (seconds) between automatic checkpoints.\n"
+	       "  -t max_time  maximum run time (seconds).\n", name);
+}
+
+static void print_cpu_model(void)
+{
+	FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
+	if (!cpuinfo) {
+		printf("couldn't open /proc/cpuinfo\n");
+		return;
+	}
+
+	char *line = NULL;
+	size_t len = 0;
+	while (getline(&line, &len, cpuinfo) != -1) {
+		if (strncmp(line, "model name", 10) == 0) {
+			printf("cpu %s", line);
+			break;
+		}
+	}
+	free(line);
+	fclose(cpuinfo);
 }
 
 int main(int argc, char **argv)
@@ -26,10 +53,13 @@ int main(int argc, char **argv)
 	int bench = 0;
 
 	int c;
-	while ((c = getopt(argc, argv, "bl:s:t:")) != -1)
+	while ((c = getopt(argc, argv, "bcl:s:t:")) != -1)
 		switch (c) {
 		case 'b':
 			bench = 1;
+			break;
+		case 'c':
+			print_cpu_model();
 			break;
 		case 'l':
 			log_file = optarg;
