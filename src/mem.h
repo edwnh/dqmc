d55953ced0b8f5ac804fb36c3f1079d44a0949c4
Edwin Huang <edwnh@users.noreply.github.com>
2025-03-28 18:05:32 -0400

    enable my_free

--- src/mem.h
+++ src/mem.h
@@ -12,5 +12,5 @@ struct alloc_entry {
 void *my_calloc(const size_t size);
 void *my_calloc_table(const struct alloc_entry *table, const size_t n_alloc);
 
-#define my_free(ptr) ((void)ptr) //free((ptr))
+#define my_free(ptr) free((ptr))
 #define my_copy(dest, src, N) memcpy((dest), (src), (N)*sizeof((src)[0]))
