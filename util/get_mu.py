36b166f07a3df224b958d389a901f914b6b0ce43
Edwin Huang <edwnh@users.noreply.github.com>
2025-04-01 12:07:43 -0400

    update get_mu.py to work when there is only one reference path
    if there is only one reference, mu is determined by using the charge
    compressibility and linearly extrapolating.

--- util/get_mu.py
+++ util/get_mu.py
@@ -1,72 +1,40 @@
-from glob import glob
 import sys
 import numpy as np
+from scipy.interpolate import CubicSpline
 import util
 
 
-def get_mu_n(path):
-    '''From all files in path, get chemical potential and filling info
-    Args:
-        path
-    Returns:
-        tuple (mu, density, density_err)
-    '''
-    n_sample, sign, density = \
-        util.load(path, "meas_eqlt/n_sample", "meas_eqlt/sign",
-                        "meas_eqlt/density")
-    mask = (n_sample == n_sample.max())
-    if not mask.all():
-        print(f"{path} incomplete: {mask.sum()}/{len(n_sample)}")
-    sign, density = sign[mask], density[mask]
-    nj = util.jackknife(sign, density.sum(1))
+def get_mu_n_chi(path):
+    period_eqlt, L, n_sweep_meas, N, mu, beta = util.load_firstfile(path,
+        "params/period_eqlt", "params/L", "params/n_sweep_meas", "params/N", "metadata/mu", "metadata/beta")
+    n_sample, sign, density, nn = util.load(path,
+        "meas_eqlt/n_sample", "meas_eqlt/sign", "meas_eqlt/density", "meas_eqlt/nn")
 
-    return util.load_firstfile(path, "metadata/mu")[0], nj[0], nj[1]
+    full_n_sample = n_sweep_meas*(L//period_eqlt)
+    frac = n_sample.mean()/full_n_sample
+    print(f"{path}: complete bins {(n_sample == full_n_sample).sum()}/{len(n_sample)}, samples {frac*100:.3f}%")
 
+    nj = util.jackknife_noniid(n_sample, sign, density.sum(1))
+    chij = beta*util.jackknife_noniid(n_sample, sign, density.sum(1), nn.sum(1),
+                                      f=lambda ns, s, n, nn: ((nn.T/s.T) - N*((n.T/s.T)**2)).T)
 
-def get_mu(targets, paths):
-    '''
-    Given list of taget filling levels and a list of paths,
-    find list of chemical potentials that will satisfy given targets
-    Args:
-        list of target filling levels in [0,2]
-        list of paths, usually mu_*/
-    Returns:
-        Tuple 
-        [0]: all mu, n data
-        [1]: list of mu values
-    '''
-    data = np.array([get_mu_n(path) for path in paths])
-    #sort rows by size of mu
-    data = data[np.argsort(data[:, 0])]
-    
-    mus = np.zeros(len(targets))
-    for i in range(len(targets)):
-        y = data[:, 1] - targets[i]
-
-        closest = np.abs(y).argmin()
-        j = np.clip(closest, 1, data.shape[0] - 2)
-        if j != closest:
-            print(f"warning: target {targets[i]} out of range of data")
-
-        p = np.polyfit(data[j-1:j+2, 0], y[j-1:j+2], 2)
-        r = np.roots(p)
-        mus[i] = r[np.abs(r - data[closest, 0]).argmin()]
-    
-    return data, mus
+    return mu, nj[0], nj[1], chij[0], chij[1]
 
 
 def main(argv):
     target = float(argv[1])
-    paths = argv[2:] #linux system automatic path expansion
-
-    data, mus = get_mu([target], paths)
-    
-    #last col:how much does filling change with each mu?
-    diffs = np.zeros(data.shape[0])
-    diffs[:-1] = data[1:, 1] - data[:-1, 1]
-    
-    print(np.hstack((data, diffs[:, None])))
-    print(mus)
+    paths = argv[2:]
+    if len(paths) == 1:
+        mu, n, n_err, chi, chi_err = get_mu_n_chi(paths[0])
+        print(mu, n, n_err, chi, chi_err)
+        target_mu = mu + (target - n)/chi
+    else:
+        data = np.array([get_mu_n_chi(path) for path in paths])
+        data = data[np.argsort(data[:, 0])] # sort rows by mu
+        print(data[:, :3])
+        cs = CubicSpline(data[:, 0], data[:, 1] - target)
+        target_mu = cs.roots(extrapolate=False)[0]
+    print(target_mu)
 
 
 if __name__ == "__main__":
--- util/print_n.py
+++ util/print_n.py
@@ -2,37 +2,36 @@ import sys
 import util
 from glob import glob
 
-def info(path):
+def get_sign_n(path):
     period_eqlt, L, n_sweep_meas = util.load_firstfile(path,
         "params/period_eqlt", "params/L", "params/n_sweep_meas")
-    full_n_sample = n_sweep_meas*(L//period_eqlt)
-
     n_sample, sign, density = util.load(path,
         "meas_eqlt/n_sample", "meas_eqlt/sign", "meas_eqlt/density")
 
+    full_n_sample = n_sweep_meas*(L//period_eqlt)
     frac = n_sample.mean()/full_n_sample
-    print(f"samples: {n_sample.mean()}/{full_n_sample}={frac*100:.2f}%")
-    print(f"complete bins: {(n_sample == full_n_sample).sum()}/{len(n_sample)}")
+    print(f"{path}: complete bins {(n_sample == full_n_sample).sum()}/{len(n_sample)}, samples {frac*100:.3f}%")
 
     if frac == 0:
         print("no data")
         return
-    mask = (n_sample > 0)
-    n_sample, sign, density = n_sample[mask], sign[mask], density[mask]
-    print(f"<sign>={util.jackknife_noniid(n_sample, n_sample, sign)}")
-    print(f"<n>={util.jackknife_noniid(n_sample, sign, density.sum(1))}")
+
+    signj = util.jackknife_noniid(n_sample, n_sample, sign)
+    nj = util.jackknife_noniid(n_sample, sign, density.sum(1))
+    return signj, nj
 
 
 def main(argv):
-    #wildcard path expansion on Windows
+    # wildcard path expansion on Windows
     for path in argv[1:]:
         paths = sorted(glob(path))
         if len(paths) == 0:
             print("No paths matching:"+ path)
         else:
             for p in paths:
-                print(p)
-                info(p)
+                signj, nj = get_sign_n(p)
+                print(f"<sign> = {signj}")
+                print(f"<n> = {nj}")
 
 if __name__ == "__main__":
     main(sys.argv)
